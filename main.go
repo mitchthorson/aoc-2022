c048c477d807d8c9359ee7213874eb14966a1211
mitchthorson <mitchell@mitchellthorson.com>
2022-12-12 00:04:59 -0500

    day 11
    wip day 12

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ import (
 	"github.com/mitchthorson/aoc-2022/day-09"
 	"github.com/mitchthorson/aoc-2022/day-10"
 	"github.com/mitchthorson/aoc-2022/day-11"
+	"github.com/mitchthorson/aoc-2022/day-12"
 )
 
 func main() {
@@ -52,5 +53,8 @@ func main() {
 	}
 	if day == "11" {
 		day11.Run()
+	}
+	if day == "12" {
+		day12.Run()
 	}
 }
