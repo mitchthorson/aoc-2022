1bc59e4d7a4f2191b853b4850d2e74ffbc2f0979
mitchthorson <mitch.thorson@protonmail.com>
2022-12-14 19:37:02 -0500

    WIP day 13

--- day-13/day-13.go
+++ day-13/day-13.go
@@ -25,17 +25,35 @@ func (s *signalPacket) Append(newPacket *signalPacket) {
 }
 
 func (s *signalPacket) String() string {
-	if len(s.Children) < 1 {
-		if s.Value == -1 {
-			return "[]"
+	output := ""
+	if s.Value >= 0 {
+		output = fmt.Sprintf("%s%d", output, s.Value)
+	}
+	if s.Value == -1 && len(s.Children) == 0 {
+		return "[]"
+	}
+	if len(s.Children) > 0 {
+		output = fmt.Sprintf("%s[", output)
+	}
+	for i, c := range s.Children {
+		if i > 0 {
+			output = fmt.Sprintf("%s,", output)
 		}
-		return fmt.Sprintf("%d", s.Value)
+		output = fmt.Sprintf("%s%s", output, c)
 	}
-	output := "["
-	for _, c := range s.Children {
-		output = fmt.Sprintf("%s %s", output, c)
+	if len(s.Children) > 0 {
+		output = fmt.Sprintf("%s]", output)
 	}
-	output = fmt.Sprintf("%s%s", output, "]")
+
+
+	// if len(s.Children) < 1 {
+	// 	if s.Value == -1 {
+	// 		return "[]"
+	// 	}
+	// 	return fmt.Sprintf("%d", s.Value)
+	// }
+	// output := "["
+	// output = fmt.Sprintf("%s%s", output, "]")
 	return output
 }
 
@@ -93,30 +111,32 @@ func (s *signalPacket) fromString(input string) *signalPacket {
 }
 
 func compare(left, right *signalPacket) int {
-	fmt.Println("comparing", left, right)
-	if left.Value > -1 && right.Value > -1 {
+	// fmt.Println("comparing", left, right)
+	// actual values, not containers
+	if len(left.Children) == 0 && len(right.Children) == 0 {
+		fmt.Println("values", left.Value, right.Value)
 		if left.Value < right.Value {
-			fmt.Println("left is smaller")
+			// fmt.Println("left is smaller")
 			return 1
 		} else if left.Value > right.Value {
-			fmt.Println("right is smaller")
+			// fmt.Println("right is smaller")
 			return -1
 		}
-		fmt.Println("equal")
+		// fmt.Println("left and right are equal")
 		return 0
 	}
 	// @ TODO this handling is all messed up now, need to fix 
-	if left.Value > -1 {
+	if len(right.Children) > 0 && len(left.Children) == 0 && left.Value != -1 {
 		// if left is an integer, put it into a list
+		fmt.Println("left is a value, right is a list, making left a sublist")
 		newVal := newPacket()
-		newVal.Value = left.Value
 		newVal.Append(&*left)
 		return compare(newVal, right)
 	}
-	if right.Value > -1 {
+	if len(left.Children) > 0 && len(right.Children) == 0 && right.Value != -1 {
 		// if right is an integer, put it into a list
+		fmt.Println("right is a value, left is a list, making right a sublist")
 		newVal := newPacket()
-		newVal.Value = right.Value
 		newVal.Append(&*right)
 		return compare(left, newVal)
 	}
@@ -136,11 +156,13 @@ func compare(left, right *signalPacket) int {
 			fmt.Println("left ran out of items")
 			return 1
 		}
+		fmt.Println("no decision, next child")
 	}
 	// in case left value is empty
-	if left.Value == -1 && len(left.Children) == 0 {
-		return 1
-	}
+	// if left.Value == -1 && len(left.Children) == 0 {
+	// 	fmt.Println("left is out of items")
+	// 	return 1
+	// }
 	// if len(left.Children) > len(right.Children) {
 	// 	return -1
 	// } else if len(right.Children) > len(left.Children) {
@@ -156,9 +178,9 @@ func parseInput(input string) [][2]*signalPacket {
 	// var parsedPair [2]signalPacket
 	for i, pair := range pairs {
 		lines := strings.Split(pair, "\n")
-		leftPackets := new(signalPacket)
+		leftPackets := newPacket()
 		leftPackets.fromString(lines[0])
-		rightPackets := new(signalPacket)
+		rightPackets := newPacket()
 		rightPackets.fromString(lines[1])
 		parsedPairs[i][0] = leftPackets
 		parsedPairs[i][1] = rightPackets
@@ -170,19 +192,21 @@ func GetResult1(input string) int {
 	pairs := parseInput(input)
 	result := 0
 	for i, p := range pairs {
-		fmt.Println("pair", i+1)
-		fmt.Println(p[0])
-		fmt.Println(p[1])
-		if compare(p[0], p[1]) > 0 {
-			fmt.Println("correct", i + 1)
-			result += (i + 1)
+		if i == 50 {
+			fmt.Println("pair", i+1)
+			fmt.Println(p[0])
+			fmt.Println(p[1])
+			if compare(p[0], p[1]) > 0 {
+				fmt.Println("correct", i + 1)
+				result += (i + 1)
+			}
 		}
 	}
 	return result
 }
 
 func Run() {
-	input := utils.ReadFile("./day-13/test_input.txt")
+	input := utils.ReadFile("./day-13/input.txt")
 	fmt.Printf("Day 13 part 1 result is:\n%d\n", GetResult1(input))
 	// fmt.Printf("Day 13 part 2 result is:\n%d\n", GetResult2(input))
 }
