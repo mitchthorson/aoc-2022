38f9dfc36fba9e093ea45a6ec52b594ac509f46b
mitchthorson <mitchell@mitchellthorson.com>
2022-12-16 21:33:42 -0500

    setup day 15 files

---
+++ day-15/day-15.go
@@ -0,0 +1,16 @@
+package day15
+
+import (
+	"fmt"
+	"github.com/mitchthorson/aoc-2022/utils"
+)
+
+func GetResult1(input string) int {
+	return 0
+}
+
+func Run() {
+	input := utils.ReadFile("./day-14/input.txt")
+	fmt.Printf("Day 15 part 1 result is:\n%d\n", GetResult1(input))
+	// fmt.Printf("Day 15 part 2 result is:\n%d\n", GetResult2(input))
+}
